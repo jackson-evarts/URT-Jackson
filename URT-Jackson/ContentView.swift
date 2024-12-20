e1ad3a1b58cae3f00e2b3a06985c87bd501f75cd
jackson-evarts <jack.evarts@gmail.com>
2024-12-20 11:13:25 -0500

    Initial Commit

---
+++ URT-Jackson/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  URT-Jackson
+//
+//  Created by Jackson Evarts on 12/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URT-Jackson/URT_JacksonApp.swift
@@ -0,0 +1,17 @@
+//
+//  URT_JacksonApp.swift
+//  URT-Jackson
+//
+//  Created by Jackson Evarts on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct URT_JacksonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
